d3e85228d688daeb6e87e3a9f09425ed8bececaa
Steven Pautz <spautz@gmail.com>
2021-04-01 21:46:38 -0400

    Add minimal test, to validate test coverage checks

---
+++ packages/accumulate-rules/tests/TODO.test.ts
@@ -0,0 +1,7 @@
+import { defaultRuleAccumulator } from '../src';
+
+describe('defaultRuleAccumulator', () => {
+  test('@TODO', () => {
+    expect(typeof defaultRuleAccumulator).toEqual('function');
+  });
+});
